64da3c667f6a2dc53b0487bf8a47e05faf27b0c2
dande011 <dande011@ucr.edu>
2013-10-08 00:25:31 -0700

    Created lab2_ex3.cpp

---
+++ labs/lab02/lab2_ex3.cpp
@@ -0,0 +1,30 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file lab02/lab2_ex2.cpp 
+/// @brief Lab 2 Excercise 3 for CS 10 Fall 2013
+///
+/// @author David Anderson [dande011@ucr.edu]
+/// @date October 8, 2013
+///
+/// @par Enrollment Info
+///     Lecture Section: 001
+/// @par 
+///     Lab Section: 028
+/// @par 
+///     TA: Mahmudal Hasan
+///
+/// @par Plagiarism Section
+/// I hereby certify that the code in this file
+/// is ENTIRELY my own original work.
+//  ================== END ASSESSMENT HEADER ===============
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+
+	return 0;
+}
