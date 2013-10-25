e0bf3cfb298290d256455a403c731f17692f6406
dande011 <dande011@ucr.edu>
2013-10-25 12:19:44 -0700

    Created cipher.cpp

---
+++ assignments/assignment 05/cipher.cpp
@@ -0,0 +1,29 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file assignment<05/cipher.cpp 
+/// @brief Assignment/Lab 05 for CS 10 Fall 2013
+///
+/// @author David Anderson [dande011@ucr.edu]
+/// @date 10-25-2013
+///
+/// @par Enrollment Notes 
+///     Lecture Section: 001
+/// @par
+///     Lab Section: 028
+/// @par
+///     TA: Mahmudul Hasan
+///
+/// @par Plagiarism Section
+/// I hereby certify that the code in this file
+/// is ENTIRELY my own original work.
+//  =============== END ASSESSMENT HEADER ==================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+return 0;
+}
