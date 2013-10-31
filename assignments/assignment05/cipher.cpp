1fb4448292d46117df93ee2711220de071940a4c
dande011 <dande011@ucr.edu>
2013-10-31 02:33:32 -0700

    Finished cipher.cpp w/ 100%.

--- assignments/assignment05/cipher.cpp
+++ assignments/assignment05/cipher.cpp
@@ -22,32 +22,82 @@ using namespace std;
 
 int main()
 {
-    string method, map;
+    string method, map, word, indexer1, indexer2;
+    const string alpha = "abcdefghijklmnopqrstuvwxyz";
+    int wordSize;
+    bool valid;
+
+    //<test>
+
+    // method = "encryption";
+    // method = "decryption";
+    // map = "default";
+    // word = "abc";
+    // word = "zyx";
+    // word = "ABC";
+
+    //</test>
+
+
     cout << "What is the method? (encryption or decryption)? ";
     cin >> method;
+    cout << endl;
 
-    if ((method=="encryption") || (method=="decryption"));
+    if (method != "encryption" && method != "decryption")
     {
-        cout << "Correct choice" << endl;
-    }
-    else
-    {
-    	cout << "Error: invalid method choice" << endl;
+        cout << "Error: invalid method choice." << endl;
+        return 0;
     }
 
 
     cout << "What is the translation map (type 'default' to ues default): ";
     cin >> map;
+    cout << endl;
 
-    if (map.size() != 26)
+    if(map=="default")
+        map = "zyxwvutsrqponmlkjihgfedcba";
+    else if (map.size() != 26)
     {
         cout << "Error: invalid translation map size." << endl;
-        return 1;
+        return 0;
     }
 
 
+    cout << "What is the single word to translate: ";
+    cin >> word;
+    cout << endl;
+    wordSize = word.size();
 
+    for(int x = 0; x < wordSize; ++x)
+    {
+        if(method=="encryption"&&(word.at(x)>=97&&word.at(x)<=122));
+        else if(map.find(word.at(x))==string::npos)
+        {
+            cout << "Error: " << method << " cannot be performed.";
+            return 0;
+        } 
+    }
+    // cout << valid << endl;
 
+    if(method=="encryption")
+    {
+        indexer2 = alpha;
+        indexer1 = map;
+        cout << "Encrypted word: ";
+    }
+    else if(method=="decryption")
+    {
+        indexer2 = map;
+        indexer1 = alpha;
+        cout << "Decrypted word: ";
+    }
+
+    for(int y = 0; y < wordSize; ++y)
+    {
+        word.at(y) = indexer1.at(indexer2.find(word.at(y)));
+    }
+
+    cout << word << endl;
 
     return 0;
 }
