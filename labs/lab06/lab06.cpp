8c90de8ec8747cad39b6ddb25eaf5cb359ef42bc
dande011 <dande011@ucr.edu>
2013-10-31 19:43:39 -0700

    Added framework for lab06.coo

---
+++ labs/lab06/lab06.cpp
@@ -0,0 +1,60 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file lab06/lab6.cpp 
+/// @brief Lab 6 for CS 10 Fall 2013
+///
+/// @author David Anderson [dande011@ucr.edu]
+/// @date October 31, 2013
+///
+/// @par Enrollment Info
+/// Lecture Section: 001
+/// @par 
+/// Lab Section: 028
+/// @par 
+/// TA: Mahmudal Hasan
+///
+/// @par Plagiarism Section
+/// I hereby certify that the code in this file
+/// is ENTIRELY my own original work.
+//  ================== END ASSESSMENT HEADER ===============
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+int excercise1()
+{
+    
+    return 0;
+}//</excercise1>
+
+int excersise2()
+{
+
+    return 0;
+}//</excercise2>
+
+int excercise3()
+{
+
+    return 0;
+}//</excercise3>
+
+
+
+int main()
+{
+    int excercise;
+    cout << "Which excercise? "
+    cin >> excersise;
+    cout << endl;
+
+    if(excersise==1)
+        exercise1();
+    else if (excercise)
+        excercise2();
+    else if (excercise)
+        excercise3();
+
+    return 0;
+}
