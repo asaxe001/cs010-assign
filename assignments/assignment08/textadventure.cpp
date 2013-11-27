a887303639c5f1df385f93828898a1e57c63a052
dande011 <dande011@ucr.edu>
2013-11-27 02:19:55 -0800

    Added extra assignment & assignmen09 100%

--- assignments/assignment08/textadventure.cpp
+++ assignments/assignment08/textadventure.cpp
@@ -28,39 +28,144 @@ void monsterRoom(int&, int&, string);
 void genieRoom(int&, int&);
 void pictureRoom(); //drawPicture();
 void drawPicture();
+void endRoom(int, int);
+void randomize(char&, char&, char&, char&);
+void userInput(char&);
+void setup(string&, string&);
 
 /// @brief Updates the inventory
 /// @param item the item in the inventory to update
 /// @param add the ammount to item (positive or negative)
 /// @param set the value to item if value passed in
-/// @return new value of item
-int inventory(int item, int add, int set=42)
+/// @return nothing
+void inventory(int& item, int add, int set=42)
 {
     item += add;
     if(set != 42)
     {
         item = set;
     }
-    return item;
+}
+
+/// @brief Prints the player status to the console;
+/// @param name the name of the player
+/// @param bananas the number of bananas
+/// @param oranges the number of oranges
+/// @return nothing
+void status(string name, int bananas, int oranges)
+{
+    cout << name << ", you are in a room with 4 doors." << endl;
+    cout << "You are carrying " << bananas << " bananas and " << oranges 
+        << " oranges." << endl << endl << endl;
 }
 
 int main()
 {
-    srand(500);
+    srand(time(0));
     int bananas=5, oranges=3;
-    string name, monster;
+    string name, scaryMonster;
+    char input;
+    char monster, genie, picture, end;
 
-    cout << "What is your name? ";
-    cin >> name;
-    cout << endl;
+    setup(name, scaryMonster);
 
-    cout << "What is the scariest monster you can think of? ";
-    cin >> monster;
-    cout << endl;
+   
+    startL:
+    randomize(monster, genie, picture, end);
+
+    input=0;
+
+    status(name, bananas, oranges);
+    userInput(input);
+
+    if(input==monster)
+        monsterRoom(bananas, oranges, scaryMonster);
+    else if(input==genie)
+        genieRoom(bananas, oranges);
+    else if(input==picture)
+        pictureRoom();
+    else if(input==end)
+    {
+        endRoom(bananas, oranges);
+        return 0;
+    }
+
+
+
+
+
+    goto startL;
+
+
+
 
 
 
     return 0;
+}
+
+/// @brief gets the initial input for setup of the game
+/// @param name the name of the player
+/// @param monster the scariest monster
+/// @return nothing
+void setup(string& name, string& monster)
+{
+    cout << "Please enter your name: ";
+    cin >> name;
+    cout << endl << endl << endl;
+
+    cout << "Name your scariest monster: ";
+    cin >> monster;
+    cout << endl << endl << endl;
+}
+
+/// @brief gets the user input
+/// @param input the char input of the user
+/// @return nothing
+void userInput(char& input)
+{
+    while(!(input=='N' || input=='S' || input=='E' || input=='W'))
+    {
+        cout << "Pick a door to enter by typing "
+            << "the direction it is in (N/E/S/W): ";
+        cin >> input;
+        cout << endl << endl << endl;
+    }
+
+}
+
+/// @brief Final output of the game at the end
+/// @param bananas the number of bananas
+/// @param oranges the number of oranges
+/// @return nothing
+void endRoom(int bananas, int oranges)
+{
+    cout << "You found the exit!" << endl;
+    cout << "Your score is " << bananas + oranges << " ("
+        << bananas << " bananas and " << oranges << " oranges)." << endl;
+    cout << "Bye bye!!!" << endl;
+}
+
+/// @brief Sets the values of the rooms to one of the two sets
+/// @return nothing
+void randomize(char& monster, char& genie, char& picture, char& end)
+{
+    switch(rand() % 2)
+    {
+        case 0:
+            monster = 'N';
+        genie   = 'S';
+        picture = 'E';
+        end     = 'W';
+        break;
+        case 1:
+            end     = 'N';
+        monster = 'S';
+        genie   = 'E';
+        picture = 'W';
+        break;
+    }
+
 }
 
 /// @brief Reduces inventory to 0, and cout attacked
@@ -70,10 +175,12 @@ int main()
 /// @return nothing
 void monsterRoom(int& bananas, int& oranges, string monster)
 {
-    bananas = inventory(bananas, 0, 0);
-    oranges = inventory(oranges, 0, 0);
-    cout "Watch out!!" << endl;
-    cout << monster << " attacks you and steals all of your bananas and oranges." << endl;
+    inventory(bananas, 0, 0);
+    inventory(oranges, 0, 0);
+    cout << "Watch out!!" << endl;
+    cout << monster
+        << " attacks you and steals all of your bananas and oranges."
+        << endl << endl << endl;
 }
 
 /// @brief Increases inventory items by specified amount
@@ -82,14 +189,18 @@ void monsterRoom(int& bananas, int& oranges, string monster)
 /// @return nothing
 void genieRoom(int& bananas, int& oranges)
 {
-    bananas = inventory(bananas, 2);
-    oranges = inventory(oranges, 1);
+    cout << "!!POOF!!" << endl;
+    cout << "A genie pops out and grants you 2 bananas and 1 orange."
+        << endl << endl << endl;
+    inventory(bananas, 2);
+    inventory(oranges, 1);
 }
 
 /// @brief Calls the drawPicture() function
 /// @return Nothing
 void pictureRoom()
 {
+    cout << "You found a picture!" << endl;
     drawPicture();
 }
 
@@ -111,3 +222,4 @@ void drawPicture()
     cout << endl;
     return;
 }
+// comment for comments sake
